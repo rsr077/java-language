597bebe43ceeb51e2f61351e832b434562578e69
rsr077 <yourfriend77777777@gmail.com>
2024-05-28 19:23:31 +0530

    new

---
+++ Java_classes/Loops/Loop.java
@@ -0,0 +1,12 @@
+
+
+public class Loop {
+public static void main(String[] args) {
+    for(int counter =0;counter<15;counter =counter +1){
+        System.out.println("Hello World");
+  
+    }
+  
+}    
+}
+
