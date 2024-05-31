03a57c5fc0639086be8a7fbc5cf8f69108345fc0
rsr077 <yourfriend77777777@gmail.com>
2024-05-31 13:03:09 +0530

    new

---
+++ Java_classes/Loops/Pattern.java
@@ -0,0 +1,16 @@
+public class Pattern {
+    public static void main(String[] args) {
+        int n =4;
+      //  int m =5;
+
+        // outer loop 
+  for (int i =2; i<=n; i++){
+     
+    // inner loop
+    for (int  j =2; j<=m; j++){
+        System.out.print("*");
+    }
+    System.out.println();
+  }
+    }
+}
