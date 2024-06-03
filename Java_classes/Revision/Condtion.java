0dfcd2b77bcbed4e090225ffa68ba346a94e6fd3
rsr077 <yourfriend77777777@gmail.com>
2024-06-03 15:43:21 +0530

    new

---
+++ Java_classes/Revision/Condtion.java
@@ -0,0 +1,12 @@
+
+public class Condtion {
+    public static void main(String[] args) {
+        int age =30;
+        if (age>18) {
+            System.out.println("This is an adult");
+
+        }else{
+            System.out.println("This is not adult");
+        }
+    }
+}
---
+++ Java_classes/Revision/Count.java
@@ -0,0 +1,9 @@
+
+
+public class Count {
+    public static void main(String[] args) {
+        for(int i=1;i<15;i++){
+            System.out.println(i);
+        }
+    }
+}
