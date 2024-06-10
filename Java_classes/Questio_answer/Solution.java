1634d36265b1d46c36e5e2d6eb60d4ba96897d12
rsr077 <yourfriend77777777@gmail.com>
2024-06-10 18:45:36 +0530

    new

--- Java_classes/Questio_answer/Solution.java
+++ Java_classes/Questio_answer/Solution.java
@@ -1,5 +1,10 @@
 import java.util.*;
 public class Solution {
    
-    public static int Greater (int a,)
+    public static void  main(String arg[]){
+ // concatenation 
+ String sentence = "TonyStark";
+ String name =sentence.substring(0,4 );
+ System.out.println(name);
+    }
 }
