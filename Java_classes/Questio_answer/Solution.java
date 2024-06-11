0501335b850fd7a3f8752b2af61e238da740c3f2
rsr077 <yourfriend77777777@gmail.com>
2024-06-11 20:38:27 +0530

    new

--- Java_classes/Questio_answer/Solution.java
+++ Java_classes/Questio_answer/Solution.java
@@ -1,10 +1,40 @@
 import java.util.*;
+
 public class Solution {
-   
-    public static void  main(String arg[]){
- // concatenation 
- String sentence = "TonyStark";
- String name =sentence.substring(0,4 );
- System.out.println(name);
+public static void main(String[] args) {
+    Scanner SC = new Scanner (System.in);
+    int rows = SC.nextInt();
+    int cols = SC.nextInt();
+
+    int [][] numbers =new int [rows][cols];
+    // input 
+    // rows
+
+    for (int i=0; i<rows ; i++){
+        // colums 
+        for (int j=0;j<cols;j++){
+     numbers[i][j]= SC.nextInt();
+        }
+    }
+   int x =SC. nextInt();
+
+    for(int i=0; i<rows; i++){
+        for(int j=0; j<cols; j++){
+           if(numbers[i][j]==x){
+            System.out.println("x found at locations (" + i + ","+j+")");
+           }
+        }
+     
     }
 }
+    
+}
+
+
+
+
+
+
+
+
+
