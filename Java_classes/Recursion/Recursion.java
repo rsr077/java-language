2f34303453ed7ca4c695dbdac9491c7bd320af7b
rsr077 <yourfriend77777777@gmail.com>
2024-06-19 20:29:01 +0530

    new

---
+++ Java_classes/Recursion/Recursion.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Recursion {
+    public static void printNumb(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+      printNumb(n-1);
+    }
+    public static void main(String[] args) {
+       int n =5;
+       printNumb(n); // n=5
+      
+ 
+        }
+    }
