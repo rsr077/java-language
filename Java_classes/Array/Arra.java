91e4c239522c711eda8ba750c78b201523e895d4
rsr077 <yourfriend77777777@gmail.com>
2024-09-15 15:33:55 +0530

    new

---
+++ Java_classes/Array/Arra.java
@@ -0,0 +1,31 @@
+
+  import java.util.*;
+
+  public class Arra {
+
+  public static void main(String[] args) {
+    Scanner SC = new Scanner(System.in);
+    int rows =SC.nextInt();
+    int cols = SC.nextInt();
+
+    int[][] numbers = new int [rows] [cols];
+
+    // input
+
+    for(int i=0; i<rows; i++) {
+      
+      for(int j=0; j<=cols; j++){
+        numbers[i][j] = SC.nextInt();
+      
+      }
+    }
+    for(int i=0; i<rows; i++){
+      for(int j=0; j<cols; j++){
+        System.out.println(numbers[i][i] +" ");
+      }
+      System.out.println();
+    }
+  }
+}
+
+
